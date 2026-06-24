ebbbe0f1502206dc49282b37aff2a685762bd355
Serge Kuznetsov <RRubys@yandex.ru>
2026-06-25 00:27:08 +0300

    initial completion of the task

--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -1,8 +1,11 @@
 package spentenergy
 
 import (
+	"errors"
 	"time"
 )
+
+var errWrongArguments = errors.New("incorrect input arguments (steps, duration, weight and height have to be positive)")
 
 // Основные константы, необходимые для расчетов.
 const (
@@ -13,17 +16,33 @@ const (
 )
 
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
+	// Проверка аргументов на положительность
+	if (steps <= 0) || (duration <= 0) || (weight <= 0) || (height <= 0) {
+		return 0, errWrongArguments
+	}
+	// Вычисление и возврат числа калорий
+	return walkingCaloriesCoefficient * (weight * MeanSpeed(steps, height, duration) * duration.Minutes()) / minInH, nil
 }
 
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
+	// Проверка аргументов на положительность
+	if (steps <= 0) || (duration <= 0) || (weight <= 0) || (height <= 0) {
+		return 0, errWrongArguments
+	}
+	// Вычисление и возврат числа калорий
+	return (weight * MeanSpeed(steps, height, duration) * duration.Minutes()) / minInH, nil
 }
 
 func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
-	// TODO: реализовать функцию
+	// Проверка, что duration > 0
+	if duration <= 0 {
+		return 0
+	}
+	// Вычисление и возврат средней скорости
+	return Distance(steps, height) / duration.Hours()
 }
 
 func Distance(steps int, height float64) float64 {
-	// TODO: реализовать функцию
+	// Вычисление и возврат дистанции
+	return stepLengthCoefficient * height * float64(steps) / mInKm
 }
